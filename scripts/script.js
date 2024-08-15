9cd392e515c6616a8207d05ae35762cd42c576f8
acellle <acellekrislette@gmail.com>
2024-08-16 04:07:55 +0800

    Added navigation bar and changed colors

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const audioButton = document.querySelector(".audio-button");
+const genshinOst = document.querySelector(".genshin-ost");
+
+function handleMusic() {
+    if (genshinOst.paused) {
+        genshinOst.play();
+        audioButton.innerHTML = `
+            <img class="play-icon" src="./assets/icons/pause-icon.png" alt="Pause Icon">
+        `;
+    } else {
+        genshinOst.pause();
+        audioButton.innerHTML = `
+            <img class="play-icon" src="./assets/icons/play-icon.png" alt="Play Icon">
+        `;
+    }
+}
+
+audioButton.addEventListener("click", () => {
+    handleMusic();
+});

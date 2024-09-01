76ba9d810dd8d103e9ef42ac40d402f2fbfcaeb3
acellle <acellekrislette@gmail.com>
2024-09-01 22:37:48 +0800

    Created footer & added footer anchor to anchor.js

--- scripts/main/anchor.js
+++ scripts/main/anchor.js
@@ -1,15 +1,17 @@
 export function handleAnchorSearch() {
   const searchBar = document.querySelector(".js-search-bar");
 
-  document.querySelector("#js-scroll-anchor").addEventListener("click", (event) => {
-    event.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    searchBar.focus();
+  document.querySelectorAll(".js-scroll-anchor").forEach((anchor) => {
+    anchor.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      searchBar.focus();
 
-    searchBar.classList.add("pulse-animation");
+      searchBar.classList.add("pulse-animation");
 
-    setTimeout(() => {
-      searchBar.classList.remove("pulse-animation");
-    }, 3000);
+      setTimeout(() => {
+        searchBar.classList.remove("pulse-animation");
+      }, 3000);
+      });
   });
 }
